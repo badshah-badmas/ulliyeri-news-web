76a64f60f38c919e45cb5f9f543eef69374ebcd2
badshah <badshahbadu62@gmail.com>
2021-12-29 01:56:58 +0530

    diractory changed

--- newsProject/settings.py
+++ newsProject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ulliyerinews.com','https://ulliyeri-news-api.herokuapp.com']
+ALLOWED_HOSTS = ['ulliyerinews.com','https://ulliyeri-news-api.herokuapp.com','127.0.0.1']
 
 CSRF_COOKIE_SECURE = True
 
